d28544e8cc399a6dc288dbae4405bba77c12f4b1
Jesen0823 <36436771+Jesen0823@users.noreply.github.com>
2021-09-12 23:13:19 +0800

    [ComponentTalk] 15. 模拟最终生成的路由 文件样板

---
+++ app/src/main/java/com/jesen/andcomponenttalk/test/ComARouter$$Group$$order.java
@@ -0,0 +1,21 @@
+package com.jesen.andcomponenttalk.test;
+
+import com.jesen.component_arouter_api.ComARouterLoadGroup;
+import com.jesen.component_arouter_api.ComARouterLoadPath;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 模拟ComARouter路由器的组文件
+ * 仅仅是样板代码，展示了预期的要生成的类的样式
+ * */
+public class ComARouter$$Group$$order implements ComARouterLoadGroup {
+
+    @Override
+    public Map<String, Class<? extends ComARouterLoadPath>> loadGroup() {
+        Map<String, Class<? extends ComARouterLoadPath>> groupMap = new HashMap<>();
+        groupMap.put("order",ComARouter$$Path$$order.class);
+        return groupMap;
+    }
+}
---
+++ app/src/main/java/com/jesen/andcomponenttalk/test/ComARouter$$Path$$order.java
@@ -0,0 +1,27 @@
+package com.jesen.andcomponenttalk.test;
+
+import com.jesen.component_annotation.model.RouterBean;
+import com.jesen.component_arouter_api.ComARouterLoadPath;
+import com.jesen.order.Order_MainActivity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 模拟ComARouter路由器的组文件对应的路径
+ * 仅仅是样板代码，展示了预期的要生成的类的样式
+ */
+public class ComARouter$$Path$$order implements ComARouterLoadPath {
+
+    @Override
+    public Map<String, RouterBean> loadPath() {
+        Map<String, RouterBean> pathMap = new HashMap<>();
+        pathMap.put("/order/Order_MainActivity", RouterBean.create(
+                RouterBean.Type.ACTIVITY,
+                Order_MainActivity.class,
+                "/order/Order_MainActivity",
+                "order")
+        );
+        return pathMap;
+    }
+}
